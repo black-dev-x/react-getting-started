b1ea49f5c3feb293d5cdc2a90db2949a5d3d4e99
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-02 00:52:07 -0300

    changing the name to setCounter

--- started/src/App.js
+++ started/src/App.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import './App.css';
 
 function Button() {
-  const [counter, updateCounter] = useState(0)
-  return <button onClick={()=>updateCounter(counter + 1)}>{counter}</button>
+  const [counter, setCounter] = useState(0)
+  return <button onClick={()=>setCounter(counter + 1)}>{counter}</button>
 }
 
 function App() {
