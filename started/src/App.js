0c7f52d05dee7e145e9b07c775c3ec7028e1c094
thiagodevx <thiago.dev.x@gmail.com>
2020-02-03 07:28:13 -0300

    Creating a mock card

--- started/src/App.js
+++ started/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.css';
 
-const App = ({title}) => (
-  <div className="header">{title}</div>
-)
+class Card extends React.Component {
+  render() {
+    return( 
+    <div className="github-profile">
+      <img src="https://placehold.it/75"/>
+      <div className="info">
+        <div className="name">Name here...</div>
+        <div className="company">Company here...</div>
+      </div>
+      One GitHub Profile...
+    </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Card></Card>
+      </div>
+    )
+  }
+}
 
 export default App;
