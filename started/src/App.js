e914fdeb02f3bb8bf68458ad5cba3766e6ae775e
thiagodevx <thiago.dev.x@gmail.com>
2020-02-03 07:33:31 -0300

    inclusion of test data

--- started/src/App.js
+++ started/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
 class Card extends React.Component {
   render() {
     return( 
@@ -10,7 +16,6 @@ class Card extends React.Component {
         <div className="name">Name here...</div>
         <div className="company">Company here...</div>
       </div>
-      One GitHub Profile...
     </div>
     )
   }
