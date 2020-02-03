0acf948bda0441791efcf419fa9007e74ff26b81
thiagodevx <thiago.dev.x@gmail.com>
2020-02-03 08:25:12 -0300

    using ref attributes

--- started/src/App.js
+++ started/src/App.js
@@ -8,10 +8,18 @@ const testData = [
 ];
 
 class Form extends React.Component {
+
+  userNameInput = React.createRef()
+
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log(this.userNameInput.current.value)
+  }
+
   render() {
     return (
-      <form action="">
-        <input type="text" placeholder="gitHub username"/>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="gitHub username" ref={this.userNameInput} required/>
         <button>Add card</button>
       </form>
     )
